3f199e400812db64469bcd75fb78491e7c8b1096
rezasaputra <rejax.adinur@gmail.com>
2017-09-30 21:41:06 +0700

    tambah

--- application/controllers/frontend/home.php
+++ application/controllers/frontend/home.php
@@ -5,16 +5,74 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$data['aktif']=1;
 		$this->load->view('frontend/head');
-		$this->load->view('frontend/menu');
+		$this->load->view('frontend/menu',$data);
+		$this->load->view('frontend/beranda/isi');
+		$this->load->view('frontend/foot');
+	}
+
+	public function selebriti()
+	{
+		$data['aktif']=2;
+		$this->load->view('frontend/head');
+		$this->load->view('frontend/menu',$data);
 		$this->load->view('frontend/beranda/isi');
 		$this->load->view('frontend/foot');
 	}
 
 	public function gayahidup()
 	{
+		$data['aktif']=3;
 		$this->load->view('frontend/head');
-		$this->load->view('frontend/menu');
+		$this->load->view('frontend/menu',$data);
+		$this->load->view('frontend/beranda/isi');
+		$this->load->view('frontend/foot');
+	}
+
+	public function kesehatan()
+	{
+		$data['aktif']=4;
+		$this->load->view('frontend/head');
+		$this->load->view('frontend/menu',$data);
+		$this->load->view('frontend/beranda/isi');
+		$this->load->view('frontend/foot');
+	}
+
+	public function kuliner()
+	{
+		$data['aktif']=5;
+		$this->load->view('frontend/head');
+		$this->load->view('frontend/menu',$data);
+		$this->load->view('frontend/beranda/isi');
+		$this->load->view('frontend/foot');
+	}
+
+	public function insani()
+	{
+		$data['aktif']=6;
+		$this->load->view('frontend/head');
+		$this->load->view('frontend/menu',$data);
+		$this->load->view('frontend/beranda/isi');
+		$this->load->view('frontend/foot');
+	}
+
+	public function solusi()
+	{
+		$data['aktif']=7;
+		$this->load->view('frontend/head');
+		$this->load->view('frontend/menu',$data);
+		$this->load->view('frontend/beranda/isi');
+		$this->load->view('frontend/foot');
+	}
+
+	
+
+	public function senyum()
+	{
+		$data['aktif']=10;
+		$this->load->view('frontend/head');
+		$this->load->view('frontend/menu',$data);
 		$this->load->view('frontend/beranda/isi');
 		$this->load->view('frontend/foot');
 	}
--- application/views/frontend/menu.php
+++ application/views/frontend/menu.php
@@ -30,9 +30,9 @@
             <div class="header--mainbar">
                 <div class="container">
                     <div class="header--logo float--left float--sm-none text-sm-center">
-                        <h1 class="h1"> <a href="home-1.html" class="btn-link"> <img width="250px" src="assets/img/logo.jpg" alt="USNews Logo"> <span class="hidden">USNews Logo</span> </a> </h1> </div>
+                        <h1 class="h1"> <a href="home-1.html" class="btn-link"> <img width="250px" src="<?=base_url();?>assets/img/logo.jpg" alt="USNews Logo"> <span class="hidden">USNews Logo</span> </a> </h1> </div>
                     <div class="header--ad float--right float--sm-none hidden-xs">
-                        <a href="#"> <img src="assets/img/ads-img/ad-728x90-01.jpg" alt="Advertisement"> </a>
+                        <a href="#"> <img src="<?=base_url();?>assets/img/ads-img/ad-728x90-01.jpg" alt="Advertisement"> </a>
                     </div>
                 </div>
             </div>
@@ -43,22 +43,32 @@
                     </div>
                     <div id="headerNav" class="navbar-collapse collapse float--left">
                         <ul class="header--menu-links nav navbar-nav" data-trigger="hoverIntent">
-                            <li class="dropdown active"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Beranda<i ></i></a>
+                            <?php 
+                                if($aktif==1){$a1="active";}else{$a1="";}
+                                if($aktif==2){$a2="active";}else{$a2="";}
+                                if($aktif==3){$a3="active";}else{$a3="";}
+                                if($aktif==4){$a4="active";}else{$a4="";}
+                                if($aktif==5){$a5="active";}else{$a5="";}
+                                if($aktif==6){$a6="active";}else{$a6="";}
+                                if($aktif==7){$a7="active";}else{$a7="";}
+                                if($aktif==8){$a8="active";}else{$a8="";}
+                                if($aktif==9){$a9="active";}else{$a9="";}
+                                if($aktif==10){$a10="active";}else{$a10="";}
+                                
+                            ?>
+                            <li class="dropdown <?=$a1;?>"> <a href="<?=base_url();?>frontend"  >Beranda<i ></i></a>
                                 
                             </li>
-                            <li class="dropdown megamenu filter"> <a href="world-news.html" class="dropdown-toggle" data-toggle="dropdown">Selebriti<i class=""></i></a>
+                            <li class="dropdown  filter <?=$a2;?>"> <a href="<?=base_url();?>frontend/home/selebriti"  >Selebriti<i class=""></i></a>
                                 
                             </li>
-                            <li><a href="<?=base_url();?>frontend/home/gayahidup">Gaya Hidup</a></li>
-                            <li><a href="<?=base_url();?>financial.html">Kesehatan</a></li>
-                            <li><a href="<?=base_url();?>entertainment.html">Kuliner</a></li>
-                            <li><a href="<?=base_url();?>lifestyle.html">Insani</a></li>
-                            <li><a href="<?=base_url();?>technology.html">Solusi</a></li>
-                            <li class="dropdown megamenu posts"> <a href="travel.html" class="dropdown-toggle" data-toggle="dropdown">Gaya Hidup<i class=""></i></a>
-                               
-                            </li>
-                            <li><a href="sports.html">Kesehatan</a></li>
-                            <li class="dropdown megamenu"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Senyum Cempaka<i class=""></i></a>
+                            <li class="dropdown <?=$a3;?>"><a href="<?=base_url();?>frontend/home/gayahidup">Gaya Hidup</a></li>
+                            <li class="dropdown <?=$a4;?>"><a href="<?=base_url();?>frontend/home/kesehatan">Kesehatan</a></li>
+                            <li class="dropdown <?=$a5;?>"><a href="<?=base_url();?>frontend/home/kuliner">Kuliner</a></li>
+                            <li class="dropdown <?=$a6;?>"><a href="<?=base_url();?>frontend/home/insani">Insani</a></li>
+                            <li class="dropdown <?=$a7;?>"><a href="<?=base_url();?>frontend/home/solusi">Solusi</a></li>
+                        
+                            <li class="dropdown  <?=$a10;?>"> <a href="<?=base_url();?>frontend/home/senyum"  >Senyum Cempaka<i class=""></i></a>
                                 
                             </li>
                             
